a39fb61e2be796a2698d03649270b3b13c345e08
dlin345 <dlin345@gmail.com>
2017-11-09 13:26:50 -0800

    Initial commit

---
+++ src/main/java/delin/ShoppingCart/App.java
@@ -0,0 +1,13 @@
+package delin.ShoppingCart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!! Let's get jiggy with it!" );
+    }
+}
